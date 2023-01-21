3c07b8ad73f22a17175bf5141302ac538bb78ca9
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-01-21 10:24:50 -0700

    all

---
+++ lesson3/scripts/l3.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('#upd').innerHTML = '&copy;' + year + '.:|:. Jesus Del Barrio .:|:. Idaho';
+
+let currentdate = document.lastModified;
+
+document.querySelector('#updates').textContent = `Last Updated: ${currentdate}`;
