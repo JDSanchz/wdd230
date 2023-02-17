e90351ff8776331adf12b8ca8339b140cb051d6b
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-02-16 19:54:16 -0700

    ddg
    dgg

---
+++ lesson7/scripts/lazyload.js
@@ -0,0 +1,24 @@
+const images = document.querySelectorAll('img');
+
+const options = {
+    threshold: 1,
+    rootMargin: '0px 0px -50px 0px'
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            const src = img.getAttribute('data-src');
+            img.setAttribute('src', src);
+            img.classList.remove('loading');
+            img.classList.add('loaded');
+            observer.unobserve(img);
+        }
+    });
+}, options);
+
+images.forEach(img => {
+    img.classList.add('loading');
+    observer.observe(img);
+});
