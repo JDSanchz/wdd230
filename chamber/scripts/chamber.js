033b68169eff670be22a803d1ddb7d408655bddf
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-02-21 07:40:33 -0700

    kk

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -43,21 +43,3 @@ if (isTuesday) {
 }
 
 
-
-// Get the current date in milliseconds
-const currentDate = new Date().getTime();
-
-// Get the last visit date from localStorage
-const lastVisitDate = localStorage.getItem('lastVisitDate');
-
-if (lastVisitDate) {
-  // Calculate the time difference in days
-  const timeDiff = (currentDate - parseInt(lastVisitDate)) / (1000 * 60 * 60 * 24);
-  const daysSinceLastVisit = Math.round(timeDiff);
-
-  // Display the message to the user
-  document.querySelector("#visitdate").innerHTML= `It's been ${daysSinceLastVisit} days since your last visit.`;
-}
-
-// Store the current date in localStorage for the next visit
-localStorage.setItem('lastVisitDate', currentDate.toString());
